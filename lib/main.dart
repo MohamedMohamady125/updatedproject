c786f4d71dfda1e98fa3cc6b3d4a7c4177406563
ymarghany21 <yehiaseif2005@gmail.com>
2025-01-20 09:46:40 -0800

    welcome screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/welcome_page.dart'; // Import the WelcomePage
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('welcome to aqua plus'),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlue,
-        ),
-        body: Center(
-          child: Text(
-            'hello',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.blue[600]!,
-              fontFamily: 'Inter',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Button Pressed'); // Add any action here
-          },
-          child: Text('press'),
-        ),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Swimming App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    ));
+      home: WelcomePage(), // Set WelcomePage as the home screen
+    );
+  }
+}
